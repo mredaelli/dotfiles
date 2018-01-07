52bcd3123c8626ca7281695ddfd1df36eeca9697
m.redaelli <m.redaelli@gmail.com>
2018-01-07 19:06:59 +0100

    gimli mods

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,7 +1,5 @@
 alias cpvpn='pushd ~/.ssh; and sudo openvpn --config CPMassimoRedaelli-TO-cp-fw-01.ovpn; or  popd' 
 
-alias kitty='python3 ~/bin/kitty' 
-
 alias ls='exa' 
 alias ll='exa -l --git'
 
@@ -15,3 +13,9 @@ alias psg='ps au | grep'
 source "$HOME/.homesick/repos/homeshick/completions/homeshick.fish"
 
 set PATH ~/bin $PATH
+
+# bobthefish
+set -g theme_title_display_process yes
+# set -g theme_color_scheme terminal-dark
+set -g theme_color_scheme solarized-dark
+# set -g theme_color_scheme dark
